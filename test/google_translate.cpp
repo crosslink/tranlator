9d7a9b912052dfc0fd44bd3736632e27ec69bb54
Eric Tang <lingxiang.tang@gmail.com>
2012-04-12 10:08:48 -0400

    ready for debugging

--- test/google_translate.cpp
+++ test/google_translate.cpp
@@ -6,8 +6,12 @@
  */
 
 #include "../src/google_translator.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char **argv) {
+	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
 
