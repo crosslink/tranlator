e2a12d7f256983345e8dbc5e815283442cb77bfe
Eric Tang <lingxiang.tang@gmail.com>
2012-04-12 09:31:39 -0400

    added autotool configs

---
+++ test/google_translate.cpp
@@ -0,0 +1,13 @@
+/*
+ * google_translate.cpp
+ *
+ *  Created on: Apr 12, 2012
+ *      Author: monfee
+ */
+
+#include "../src/google_translator.h"
+
+int main(int argc, char **argv) {
+	return 0;
+}
+
